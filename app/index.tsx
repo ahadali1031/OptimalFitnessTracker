aa4d6d2005350077cb58fbce55196f57dbd09fe2
Ahad Ali <aali333@gatech.edu>
2025-10-19 16:00:39 -0700

    Added 5 exercises with ability to press and bolden them. The exercises contain the name and muscle groups which they target.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { Text, View, Pressable } from "react-native";
+import { Muscle } from "../constants/muscles"
+import { useState } from "react";
 
 export default function Index() {
+
+  const[selectedExerciseID, setSelectedExerciseID] = useState<string | null>(null);
+
+  const exercises = [
+    {id: "back-squat", name: "Back Squat", muscleGroups: [Muscle.QUADS, Muscle.GLUTES, Muscle.HAMSTRINGS]},
+    {id: "leg-extensions", name: "Leg Extensions", muscleGroups: [Muscle.QUADS]},
+    {id: "leg-curls", name: "Leg Curls", muscleGroups: [Muscle.HAMSTRINGS]},
+    {id: "calf-raises", name: "Calf Raises", muscleGroups: [Muscle.CALVES]},
+    {id: "bulgarian-split", name: "Bulgarian Split Squat", muscleGroups: [Muscle.QUADS, Muscle.HAMSTRINGS, Muscle.GLUTES]},
+  ]
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit.</Text>
-    </View>
+    <View>
+      { exercises.map(exercise => {
+        const isActive = exercise.id == selectedExerciseID;
+        return (
+          <Pressable key={exercise.id} onPress={() => setSelectedExerciseID(exercise.id)}>
+            <Text style = {{ fontWeight: isActive ? "bold" : "normal" }}> 
+              {exercise.name}
+            </Text>
+          </Pressable>
+        );
+      })}
+    </View> 
   );
 }
---
+++ constants/muscles.ts
@@ -0,0 +1,7 @@
+export enum Muscle {
+    QUADS = "quads",
+    HAMSTRINGS = "hamstrings",
+    CALVES = "calves",
+    GLUTES = "glutes"
+
+}
